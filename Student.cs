e401f9564048964b422a92946364993c49b0a2cb
PlatonZvonkov <60015861+PlatonZvonkov@users.noreply.github.com>
2020-03-13 21:37:17 +0300

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -6,7 +6,21 @@ namespace NoobApp
 {
     public class Student
     {
-        public int id { get; set; }
+        private int _id;
+        public int id { get { return _id; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Внимание Некоторые id внесены с отрицательными значениями!" +
+                        "и не будут активированны!");
+                }
+                else
+                {
+                    _id = value;
+                }
+            }
+        }
         public string Name { get; set; }
         public int Score { get; set; }
     }
